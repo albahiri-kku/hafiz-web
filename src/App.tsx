cc11f3d742f66ec054e20b3e5fea067d4c812729
albahiri-kku <albahiri@gmail.com>
2026-04-25 11:33:49 +0300

    Revert "feat: صفحة /login كبوابة دخول متعدّدة الجهات + ربطها بالـ Navbar"
    This reverts commit 84ad042af66da365769b27cb179e1b7db68efa18.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import MaqraaPage from "./pages/MaqraaPage";
-import Login from "./pages/Login";
 import "./index.css";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/maqraa" element={<MaqraaPage />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
